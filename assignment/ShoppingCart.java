b8dc10876b5377e931c6270e91aae3091f6138bc
hari-lingayala <harilingayala@gmail.com>
2026-07-17 11:13:19 +0530

    word-frequency counter

---
+++ assignment/ShoppingCart.java
@@ -0,0 +1,26 @@
+public class ShoppingCart{
+    private List<Product> items = new arraylist<>();
+    public void oddproduct(Product product)
+    items.oddproduct ;
+}
+
+//acessing elements 
+String first = students.get(0);
+String third = students.get(3);
+System.out.println("First : "+ first","third: "+ third");
+
+students.remove(1);
+students.remove("bob");
+System.out.println("after removeIf" : +students");
+
+boolean hasAlice = students.contains("Aliice");
+boolean hasBob = students.contains("Bob");
+System.out.println("Contains Alice ? " +hasAlice);
+System.out.println("Contains Bob ? " +hasBob);
+
+int size = students.size();
+boolean empty = studens.isempty();
+System.out.println("student #"1+":"+nstudents.get(i));
+}
+System.out.println
+
---
+++ assignment/WordFrequency.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class WordFrequency {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a sentence:");
+        String sentence = sc.nextLine();
+
+        String[] words = sentence.toLowerCase().split("\\s+");
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        System.out.println("\nWord Frequencies:");
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+
+        sc.close();
+    }
+}
